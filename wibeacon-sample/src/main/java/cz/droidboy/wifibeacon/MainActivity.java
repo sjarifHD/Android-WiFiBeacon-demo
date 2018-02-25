0703f76c6bf10038cbbca0a9f7bd4bea2c5cc8f4
Syarif Hidayat <sjarif.hd@gmail.com>
2018-02-25 18:46:58 +0700

    fix(gradle): downgrade buildtools for development which is ignore permission

--- wibeacon-sample/src/main/java/cz/droidboy/wifibeacon/MainActivity.java
+++ wibeacon-sample/src/main/java/cz/droidboy/wifibeacon/MainActivity.java
@@ -22,7 +22,6 @@ import cz.droidboy.wibeacon.range.ScanFilter;
  * @author Jonas Sevcik
  */
 public class MainActivity extends BaseActivity implements ProximityScanner.RangingListener {
-    private static final int PERMISSION_REQUEST_CODE = 101;
 
     private ProximityScanner scanner;
     private APAdapter adapter;
@@ -31,15 +30,6 @@ public class MainActivity extends BaseActivity implements ProximityScanner.Rangi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!checkPermission()) {
-                String[] permission = {Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                };
-                requestPermissionsSafely(permission, PERMISSION_REQUEST_CODE);
-            }
-        }
 
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setEmptyView(findViewById(android.R.id.empty));
@@ -87,23 +77,4 @@ public class MainActivity extends BaseActivity implements ProximityScanner.Rangi
             return super.onOptionsItemSelected(item);
         }
     }
-
-    private boolean checkPermission() {
-        return hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-                && hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    public void requestPermissionsSafely(String[] permissions, int requestCode) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(permissions, requestCode);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    public boolean hasPermission(String permission) {
-        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
-                checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
-    }
-
 }
